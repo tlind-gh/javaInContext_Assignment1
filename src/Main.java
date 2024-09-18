d41d3cd05d144f8f1b4c17b5968ab36dc9c4433d
Teo Lind <teo.l.lind@gmail.com>
2024-09-18 17:00:42 +0200

    minor changes to comments

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-//T.Lind, Sep2024
+//T.Lind, 2024Sep18
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -7,10 +7,11 @@ public class Main {
     public static void main(String[] args) {
 
         /*General note: all user input is passed via methods (located below main method) which catch invalid
-        user input and loops until the user provides a valid input for the current prompt*/
+        user input and loops asking the user for new input until the user provides a valid input*/
 
         /*variable for account balance, starts at zero and can be changed at program start-up
-        (would, in an actual program of this type, be imported from memory/external source)*/
+        (would, in an actual program of this type, be imported from memory/external source and not subject to
+        direct change by user)*/
         double accountBalance = 0;
 
         //greet user and inform of current account balance
@@ -21,6 +22,7 @@ public class Main {
         in a real accounting software, but is included here to be able to set different amounts during testing
         of the program w/o altering the code)*/
         System.out.print("Is balance correct (Y/N)?: ");
+        //method (as if-condition) only allowing y/n/yes/no as user input
         if (!userInputYesNo()) {
             System.out.print("Input correct balance: ");
             accountBalance = userInputPosDouble("Input must be a positive number. Try again: ");
@@ -28,9 +30,9 @@ public class Main {
         }
 
         /*do-while loop for the main menu (and the different actions/options within it) that keeps the program
-        running until the user actively chooses to exit the program*/
+        running until the user actively chooses to exit the program (by pressing -1 in main menu)*/
 
-        //variable for holding user input in main menu
+        //variable for holding user input in main menu (used for while condition)
         int choice;
 
         do {
@@ -40,14 +42,16 @@ public class Main {
             System.out.println("3. Pay invoices (press 3)");
             System.out.print("Choose option (or press -1 to exit program): ");
 
-            //receive user input and jump to correct code section using a switch expression
+            //method only allowing 1,2,3 or -1 as input
             choice = userInputMenu();
+            //switch expression to jump to correct code section based on user input
             switch (choice) {
 
                 case 1:
                     //confirm the user choice via print statement and ask for nr of employees
                     System.out.println("\nPAY EMPLOYEE SALARIES");
                     System.out.print("Input number of employees: ");
+                    //method only allowing positive integer numbers
                     int nrOfEmployees = userInputPosInt();
 
                     //list to store employee salaries
@@ -55,10 +59,11 @@ public class Main {
                     //variable for holding sum of all salaries after tax(-30%)
                     double netSumSalaries = 0;
 
-                    //loop the list and store salaries (from user input)
+                    //loop for storing user input in salaries list and adding up them to total net sum (-30% tax)
                     System.out.println("Input the salary for each employee");
                     for (int i = 0; i < nrOfEmployees; i++) {
                         System.out.print("Employee " + (i + 1) + ": ");
+                        //method only allowing numbers >= 0
                         salaries[i] = userInputPosDouble("Input must be a positive number.\nEmployee"+ (i+1) +": ");
                         netSumSalaries += salaries[i]*0.7;
                     }
@@ -83,12 +88,12 @@ public class Main {
                     System.out.print("Input the cost to be invoiced: ");
                     double costOutgoingInvoice = userInputPosDouble("Input must be a positive number.\nTry again: ");
 
-                    //store the VAT tax in a separate variable
+                    //store the VAT tax in a separate variable for ease of use
                     double vat = costOutgoingInvoice * 0.20;
                     //add the invoiced amount (w/o VAT) to the account balance
                     accountBalance += (costOutgoingInvoice - vat);
 
-                    //inform user of details of the created invoice and the new account balance
+                    //print summary of the created invoice and the new account balance after invoicing
                     System.out.println("\nTotal: " + costOutgoingInvoice);
                     System.out.println("VAT: " + vat);
                     System.out.println("Total after VAT deduction: " + (costOutgoingInvoice - vat));
@@ -98,11 +103,13 @@ public class Main {
                 case 3:
                     //confirm the user choice via print statement and ask for nr of invoices to be paid
                     System.out.println("\nPAY INVOICES");
-                    System.out.println("Input the number of invoices to be payed: ");
+                    System.out.print("Input the number of invoices to be payed: ");
                     int nrOfInvoices = userInputPosInt();
 
                     /*variable for storing user input. NB! not stored in a list, since we only need the sum
-                    of all invoices and individual invoice amounts will not be used by the program again*/
+                    of all invoices and individual invoice amounts will not be used by the program again.
+                    deviation from the assigment specification, but storing and reading lists is included in case 1
+                    so I thought it would be okay to exclude*/
                     double costIncomingInvoice;
                     //variable for holding sum (with VAT tax deducted) of all invoices
                     double netSumInvoices = 0;
@@ -144,17 +151,17 @@ public class Main {
     }
 
 
-    /*General info userInput methods (i.e., all methods except the "makePayment()"-method:
-    1. All methods userInput methods use a while-loop where while is always true. The loops are broken with
+    /*GENERAL INFO userInput METHODS:
+    1. All userInput methods use a while-loop where while is always true. The loops are broken with
     break statements which are only reached after user has given correct/valid input (method dependent)
-    2. All methods userInput methods except userInputYesNo() has a try/catch expression to handle user input that is of incorrect data type
-    (not int or not double, depending on method). userInputYesNo() stores the input in a String and can therefor
-    handle any user input without java giving an error.*/
+    2. All userInput methods except userInputYesNo() has a try/catch expression to handle user input that is of
+    incorrect data type (i.e., not int or not double, depending on method which gives InputMismatchException in java).
+    userInputYesNo() stores the input in a String and can therefor handle any user input without java giving an error.*/
 
 
     static boolean userInputYesNo() {
         /*method for handling user input for yes/no-prompts within the main program. returns a boolean which is
-        true or false according to user input (yes = true, no = false)*/
+        true or false according to user input*/
 
         //boolean needs to be set to false or true to not give an error for the return statement
         boolean isYes = false;
@@ -164,7 +171,7 @@ public class Main {
         while (true) {
             String inputString = sc.nextLine();
             /*convert to lowercase to make the method upper/lowercase insensitive (i.e., it does not matter for the
-            method output if user types in lower or uppercase*/
+            method output if user types in lower or uppercase, it is treated the same*/
             inputString = inputString.toLowerCase();
 
             //methods accepts both full words (yes/no) and single characters (y/n) and sets boolean accordingly
@@ -176,7 +183,7 @@ public class Main {
                     break;
                 default:
                     /*if input is not one of the case option, ask the user to try again and continues to next iteration
-                    of loop (thus not reaching the break statement below and cont. while loop)*/
+                    of loop (thus not reaching the break statement below and continues the while loop)*/
                     System.out.print("Please press Y or N: ");
                     continue;
             }
@@ -213,14 +220,14 @@ public class Main {
     }
 
     static int userInputPosInt() {
-        //method for handling user input that needs to be a positive integer
+        //method for handling user input that must be a positive integer
 
         int inputInt;
-        //message to user when entering invalid input
-        String s = "Input must be a positive whole number. Try again: ";
+        //message to user when entering invalid input (stored in variable for ease of use)
+        String userErrorMessage = "Input must be a positive whole number. Try again: ";
         Scanner sc = new Scanner(System.in);
 
-        //runs until user gives valid menu input (i.e., a whole number larger than zero)
+        //runs until user gives valid input (i.e., a whole number larger than zero)
         while (true) {
             try {
                 inputInt = sc.nextInt();
@@ -228,10 +235,10 @@ public class Main {
                 if (inputInt > 0) {
                     break;
                 } else {
-                    System.out.print(s);
+                    System.out.print(userErrorMessage);
                 }
             } catch (InputMismatchException e) {
-                System.out.print(s);
+                System.out.print(userErrorMessage);
                 sc.nextLine();
             }
         }
@@ -240,7 +247,7 @@ public class Main {
 
     static double userInputPosDouble(String userErrorMessage) {
         //method for handling user input that needs to be a double equal to or larger than zero
-        //appropriate printed error message to user is passed into the method via a string when it is called from main program
+        //desired printed error message to user is specified when calling method (via argument)
 
         double inputDouble;
         Scanner sc = new Scanner(System.in);
@@ -265,15 +272,15 @@ public class Main {
 
     static double makePayment(double accountBalance, double cost, String costType) {
         /*method for making a payment from the account. method only allows payment if account has sufficient funds
-        the methods is called with three variables which are:
+        The methods must be called with three arguments which are:
         1. accountBalance (current account balance from main)
         2. cost (amount to be paid/withdrawn from account)
         3. costType (a string specifying what the payment is for, e.g., salaries) which is used in print statements*/
 
         System.out.print("\nPay "+ costType +" (Y/N)?: ");
-        //calls method for handling y/n user input
+        //calls another method for handling y/n user input
         if (userInputYesNo()) {
-            //check if account balance is sufficient to pay the invoices
+            //check if account balance is sufficient to pay the invoices and make payment if possible
             if (cost > accountBalance) {
                 System.out.println("Payment could not be made due to insufficient funds.\nCurrent account balance: " + accountBalance);
             } else {
